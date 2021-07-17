5882e555c8ceb7024d382060ff42402791986399
Connor Wyatt <connorwyatt1@gmail.com>
2021-07-17 19:21:48 +0100

    Add head properties

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { FC } from 'react'
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Connor &amp; Laura's Wedding</title>
+      <link rel='icon' href='/favicon.ico' />
+    </Head>
+
+    <Component {...pageProps} />
+  </>
+)
 
 export default MyApp
