5b6a8bfbde5176b00c40bc232cfe421635d1eda3
Connor Wyatt <connorwyatt1@gmail.com>
2021-08-04 00:21:34 +0100

    Make banner text bold

--- components/ThanksForRsvpBanner.styles.ts
+++ components/ThanksForRsvpBanner.styles.ts
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
 import { PaddedContainer } from './PaddedContainer.styles'
-import { boxShadow } from 'styles/helpers'
+import { boxShadow, textStyle } from 'styles/helpers'
 
 export const Container = styled(PaddedContainer)`
   ${({ theme }) => boxShadow(theme.shadows.banner)};
+  ${({ theme }) => textStyle(theme.typography.paragraphEmphasis)};
   background-color: ${({ theme }) => theme.colors.banner.background};
   color: ${({ theme }) => theme.colors.banner.text};
 `
